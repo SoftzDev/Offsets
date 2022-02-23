4b541e884d7c43c814cbc4588f71fdf55385cd6f
T0DY <100039866+T0DY@users.noreply.github.com>
2022-02-24 04:46:48 +0700

    Update and rename offsets to offsets.cpp

---
+++ offsets.cpp
@@ -0,0 +1,17 @@
+namespace offsets
+{
+    // client.dll
+    constexpr auto localPlayer = 0x;
+    constexpr auto entityList = 0x;
+    constexpr auto glowObjectManager = 0x;
+
+    // entity
+    constexpr auto teamNum = 0x;
+    constexpr auto m_bSpotted = 0x;
+    constexpr auto glowIndex = 0x;
+
+    // add-on
+    constexpr auto flags = 0x;
+    constexpr auto forceJump = 0x;
+
+}
